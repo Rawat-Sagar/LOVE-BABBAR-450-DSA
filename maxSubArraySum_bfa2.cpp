d1f7e84ecaea67cd599bc7a2ddfe942cff7b801d
Sagar Rawat <70856578+Rawat-Sagar@users.noreply.github.com>
2021-02-02 23:49:16 +0530

    Create maxSubArraySum_bfa2.cpp

---
+++ maxSubArraySum_bfa2.cpp
@@ -0,0 +1,22 @@
+// Time complexity = O(n**2)
+#include <iostream>
+using namespace std;
+
+int main(){
+   int arr_size ;
+   cout<<"Array size :";
+   cin>>arr_size;
+   int arr[arr_size];
+   for(int i=1;i<=arr_size;i++) cin>>arr[i];
+   int max = INT8_MIN;
+   for(int i=1;i<=arr_size;i++){
+      int s=0;
+     for(int j=i;j<=arr_size;j++){
+       s=s+arr[j];
+       if(s>max){
+         max = s;
+       }
+     }
+   }
+cout<<max;
+}
