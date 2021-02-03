1f8899f3e0455bf1ec0c870585649d46c0d08533
Sagar Rawat <70856578+Rawat-Sagar@users.noreply.github.com>
2021-02-03 23:33:43 +0530

    Create duplicate_array(n+1)_bfa.cpp

---
+++ duplicate_array(n+1)_bfa.cpp
@@ -0,0 +1,22 @@
+// Time Complexity : O(n**2)
+
+#include <iostream> 
+using namespace std; 
+int main()  
+{  
+    //Initialize array   
+    int arr[] = {1, 2,3,4,2};   
+      
+    //Calculate length of array arr  
+    int length = sizeof(arr)/sizeof(arr[0]);  
+      
+    printf("Duplicate elements in given array: \n");  
+    //Searches for duplicate element  
+    for(int i = 0; i < length; i++) {  
+        for(int j = i + 1; j < length; j++) {  
+            if(arr[i] == arr[j])  
+                cout<<arr[j]<<" ";  
+        }  
+    }  
+    return 0;  
+}  
