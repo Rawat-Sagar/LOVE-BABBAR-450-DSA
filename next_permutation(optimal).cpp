a38c58c83579fa68fe477382fb8f28b3985c8f62
Sagar Rawat <70856578+Rawat-Sagar@users.noreply.github.com>
2021-02-05 17:22:03 +0530

    Create next_permutation(optimal).cpp

---
+++ next_permutation(optimal).cpp
@@ -0,0 +1,43 @@
+//Time Complexity :O(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+void nextPermutation()
+{ 
+  vector<int> a;
+  a = {3,2,1};
+  int n = 3 ;
+  int idx = -1;
+  for(int i = n-1 ; i>0 ; i--)
+  {
+    if(a[i] > a[i-1])
+    {
+      idx = i;
+      break;
+    }
+  }
+  if(idx == -1){
+    reverse(a.begin(),a.end());
+  }
+  else
+  {
+    int prev = idx;
+    for(int i = idx+1 ; i<n ; i++)
+    {
+      if(a[i] >a[idx-1] and a[i]<=a[prev])
+      {
+        prev = i;
+      }
+    }
+    swap(a[idx-1],a[prev]);
+    reverse(a.begin()+idx , a.end());
+  }
+  cout<<"The next permutation is :"<<endl;
+  for(int i=0 ; i<n ; i++){
+    cout<<a[i]<<" ";
+  }
+}
+
+int main(){
+  nextPermutation();
+}
